f18a00875e94cc4c67301a5bc0ee401087368540
Elad Hamneshin <hamneshin123@gmail.com>
2023-11-09 23:28:15 +0200

    {add} simple express server config

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import morgan from 'morgan';
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+export const app = express();
+
+// APP CONFIGS
+dotenv.config();
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`server is runing at port ${port}`);
+});
