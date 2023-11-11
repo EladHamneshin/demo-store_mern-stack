963da81420d2663ad02e062bcaf2b9f0c8697b36
Elad Hamneshin <hamneshin123@gmail.com>
2023-11-11 18:06:45 +0200

    {add} userController with registerUser function

---
+++ server/src/controllers/userController.ts
@@ -0,0 +1,27 @@
+import asyncHandler from "express-async-handler";
+import { Request, Response } from "express";
+import STATUS_CODES from "../utils/StatusCodes.js";
+//import RequestError from "../types/errors/RequestError.js";
+import userService from "../services/userService.js";
+//import userValidation from "../utils/validations/userValidation.js";
+
+
+// @desc    Register a new user
+// @route   POST  /api/users/register
+// @access  Public
+const registerUser = asyncHandler(async (req: Request, res: Response) => {
+
+    //const { error } = userValidation(req.body);
+    //if (error)
+    //  throw new RequestError(error.message, STATUS_CODES.BAD_REQUEST);
+    console.log(req.body);
+    
+    const user = await userService.addUser(req.body);
+  
+    res.status(STATUS_CODES.CREATED).json({
+      _id: user._id,
+      email: user.email,
+    });
+});
+
+export default { registerUser };
