29f26b417062cc295a4611d6bc109bbc955f8069
Evyatar <revachevyatar@gmail>
2023-11-12 20:46:50 +0200

    all Categories and top 5Categories and Productscat

---
+++ server/src/services/categoryService.ts
@@ -0,0 +1,28 @@
+import categoryDal from "../dal/categoryDal.js";
+import RequestError from "../types/errors/RequestError.js";
+import STATUS_CODES from "../utils/StatusCodes.js";
+import { Request } from "express";
+
+const getCategories = async () => {
+    const categorys = await categoryDal.getCategories();
+    if (!categorys)
+        throw new RequestError('Categorys not found', STATUS_CODES.NOT_FOUND);
+    return categorys;
+}
+
+const getCategoryProducts = async (req: Request) => {
+    const { name } = req.params;
+    const category = await categoryDal.getCategoryProducts(name);
+    if (!category)
+        throw new RequestError('Category not found', STATUS_CODES.NOT_FOUND);
+    return category;
+
+}
+const get5Categories = async () => {
+    const category = await categoryDal.get5Categories();
+    if (!category)
+        throw new RequestError('Category not found', STATUS_CODES.NOT_FOUND);
+    return category;
+}
+
+export default { getCategories, getCategoryProducts, get5Categories }
