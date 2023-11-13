7b89c267e8a29445deea7149ccd8790a8a968400
Yakov Damen <yakov.damen@gmail.com>
2023-11-13 11:56:59 +0200

    {fix} spelling etc.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,32 +1,31 @@
-import cors from 'cors';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import { connectDB } from './configs/db.js';
-import { errorHandler, notFound } from './middlewares/errorsMiddleware.js';
-import userRoutes from './routes/userRoutes.js';
-import cartRoutes from './routes/cartRoutes.js';
-
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import express from "express";
+import cookieParser from "cookie-parser";
+import { connectDB } from "./configs/db.js";
+import { errorHandler, notFound } from "./middlewares/errorsMiddleware.js";
+import userRoutes from "./routes/userRoutes.js";
+import cartRoutes from "./routes/cartRoutes.js";
 
 const app = express();
 
 // APP CONFIGS
 dotenv.config();
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use('/api/users', userRoutes);
-app.use('/api/users/cart', cartRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/users/cart", cartRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 
-await connectDB()
+await connectDB();
 
 app.listen(port, () => {
-    console.log(`server is running at port ${port}`);
+  console.log(`server is running at port ${port}`);
 });
