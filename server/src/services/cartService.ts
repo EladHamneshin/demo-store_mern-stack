fedcb191f7519cf3c22b36e4a0364e684ab6d206
Yakov Damen <yakov.damen@gmail.com>
2023-11-13 12:03:18 +0200

    {add} cart service function update cart

--- server/src/services/cartService.ts
+++ server/src/services/cartService.ts
@@ -1,16 +1,34 @@
-import userDal from "../dal/userDal.js";
 import STATUS_CODES from "../utils/StatusCodes.js";
 import RequestError from "../types/errors/RequestError.js";
 import { Types } from "mongoose";
 import cartDal from "../dal/cartDal.js";
+import CartItem from "../types/CartItem.js";
+import CartItems from "../types/Cart.js";
 
 const getCart = async (userId: Types.ObjectId) => {
-	const cart = await cartDal.getCart(userId);
-	// if(!cart)
-	// 	return null;
-    console.log(cart);
-    
-	return cart;
-}
+  const cart = await cartDal.getCart(userId);
+  if (!cart) return null;
+  return cart;
+};
 
-export default { getCart };
+const updateCart = async (userId: Types.ObjectId, item: CartItem) => {
+  const dbCart: CartItems | null = await cartDal.getCartProducts(userId);
+  if (!dbCart)
+    throw new RequestError("Cart not found", STATUS_CODES.BAD_REQUEST);
+
+  const index = dbCart.items.findIndex(
+    (dbItem) => dbItem.product_id.toString() === item.product_id.toString()
+  );
+
+  if (index === -1) dbCart.items.push(item);
+  else dbCart.items.splice(index, 1, item);
+
+  const cartRes = await cartDal.updateCart(userId, dbCart.items);
+  if (!cartRes)
+    throw new RequestError(
+      "Cart update failed",
+      STATUS_CODES.INTERNAL_SERVER_ERROR
+    );
+  return cartRes;
+};
+export default { getCart, updateCart };
