3e0cbfd53c7dcbb5faf0d141d1ca2fb15df5a4e7
Yakov Damen <yakov.damen@gmail.com>
2023-11-13 12:02:13 +0200

    {add} cart-update route and auth middleware

--- server/src/routes/cartRoutes.ts
+++ server/src/routes/cartRoutes.ts
@@ -1,15 +1,14 @@
-import  express  from "express";
+import express from "express";
 import { authHandler } from "../middlewares/authMiddleware.js";
 import cartController from "../controllers/cartController.js";
 
 const cartRouter = express.Router();
 
-cartRouter.use(authHandler)
+cartRouter.use(authHandler);
 
-cartRouter.get('/', cartController.getCart);
-// cartRouter.put('/', cartController.updateCart);
+cartRouter.get("/", cartController.getCart);
+cartRouter.put("/", cartController.updateCart);
 // cartRouter.delete('/', cartController.deleteCart);
 // cartRouter.patch('/', cartController.patchCart);
-
 
 export default cartRouter;
