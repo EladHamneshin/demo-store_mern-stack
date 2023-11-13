0d5cddbe8e3390f0e01785455e1de678ba376523
Yakov Damen <yakov.damen@gmail.com>
2023-11-13 12:05:30 +0200

    {add} update func to controller and dal

--- server/src/controllers/cartController.ts
+++ server/src/controllers/cartController.ts
@@ -1,7 +1,5 @@
 import asyncHandler from "express-async-handler";
-import { Request, Response } from "express";
 import STATUS_CODES from "../utils/StatusCodes.js";
-import RequestError from "../types/errors/RequestError.js";
 import cartService from "../services/cartService.js";
 
 // @desc    Get shopping cart
@@ -12,14 +10,19 @@ const getCart = asyncHandler(async (req, res) => {
 
   res.json({
     cartId: cart?.id,
-    items: cart?.items.map((item) => 
-    item
-    ),
+    items: cart?.items.map((item) => item),
   });
 });
 
 // @desc    Update shopping cart
-// @route   PUT /api/cart
+// @route   PUT /api/users/cart
 // @access  Private
+const updateCart = asyncHandler(async (req, res) => {
+  const cart = await cartService.updateCart(req.userId, req.body);
 
-export default { getCart };
+  res.status(STATUS_CODES.CREATED).json({
+    cart: cart,
+  });
+});
+
+export default { getCart, updateCart };
--- server/src/dal/cartDal.ts
+++ server/src/dal/cartDal.ts
@@ -1,11 +1,29 @@
 import { Types } from "mongoose";
 import cartModel from "../models/cartModel.js";
 import productModel from "../models/productModel.js";
+import CartItems from "../types/Cart.js";
 
 const b = productModel.find();
+
+const createCart = async (userId: Types.ObjectId) => {
+  return await cartModel.create({ user: userId });
+};
 
 const getCart = async (userId: Types.ObjectId) => {
   return await cartModel.findOne({ user: userId }).populate("items.product_id");
 };
 
-export default { getCart };
+const getCartProducts = async (userId: Types.ObjectId) => {
+  return await cartModel.findOne({ user: userId });
+};
+const updateCart = async (
+  userId: Types.ObjectId,
+  items: CartItems["items"]
+) => {
+  return await cartModel.findOneAndUpdate(
+    { user: userId },
+    { items: items },
+    { new: true }
+  );
+};
+export default { createCart, getCart, getCartProducts, updateCart };
