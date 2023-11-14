38381b908ba9c92d00c0fc724934ba41b6ae9ff2
Evyatar <revachevyatar@gmail>
2023-11-14 13:38:31 +0200

    fix app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,12 @@ import './App.css'
 import Copyright from './components/Copyright'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import second from './api/cartsAPI'
-import usersAPI from './api/usersAPI'
-import CartPage from './pages/CartPage'
+
 function App() {
-    // usersAPI.loginUser("dani@gmail.com","Password123@") 
-    // usersAPI.logoutUser()
-    second.getCart()
+
     return (
         <>
-            <CartPage></CartPage>
+
             <ToastContainer />
             <Outlet />
             <Copyright sx={{ mt: 5 }} />
