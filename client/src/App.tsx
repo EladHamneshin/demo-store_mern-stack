e6af415089bff0cc428cd1e6dd31d6b3bcc3de85
Yakov Damen <yakov.damen@gmail.com>
2023-11-14 21:37:32 +0200

    {add} category-nav to app component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,14 @@ import Copyright from './components/Copyright'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import AppBar from './components/AppBar'
+import CategoryNav from './components/CategoryNav'
 
 function App() {
 
     return (
         <>
             <AppBar/>
+            <CategoryNav/>
             <ToastContainer />
             <Outlet />
             <Copyright sx={{ mt: 5 }} />
