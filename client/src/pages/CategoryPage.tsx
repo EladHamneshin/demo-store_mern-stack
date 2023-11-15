c01807521c2aeb2cbf7af9fe753dfe9872955ef4
Elad Hamneshin <hamneshin123@gmail.com>
2023-11-15 09:35:14 +0200

    {add} ProductCardsContainer and display products on CategoryPage

--- client/src/pages/CategoryPage.tsx
+++ client/src/pages/CategoryPage.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import Product from '../types/Product';
+import categoriesAPI from '../api/categoriesAPI';
+import ProductCard from '../components/ProductCard';
+import ProductCardsContainer from '../components/ProductCardsContainer';
 
-type Props = {}
+const CategoryPage = () => {
+  const { cname } = useParams();
+  const [products, setProducts] = useState<Product[]>([]);
 
-const CategoryPage = (props: Props) => {
+  useEffect(() => {
+    categoriesAPI.getProductsFromCategory(cname!).then((products) => {    
+      setProducts(products);
+    })
+    .catch((err) => { console.log(err); })
+  }, []);
+
   return (
-    <div>CategoryPage</div>
+  <ProductCardsContainer>
+    {products.map((product, index)=>{
+    return <ProductCard key={"cproduct"+index} product={product}/>})
+  }</ProductCardsContainer>
   )
 }
 
