5c820746df8adde6fc86821b18a31ba47aec4619
Yakov Damen <yakov.damen@gmail.com>
2023-11-16 15:55:39 +0200

    {add} filter to category page

--- client/src/pages/CategoryPage.tsx
+++ client/src/pages/CategoryPage.tsx
@@ -1,27 +1,40 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Product from '../types/Product';
 import categoriesAPI from '../api/categoriesAPI';
 import ProductCard from '../components/ProductCard';
 import ProductCardsContainer from '../components/ProductCardsContainer';
+import Filter from '../components/Filter';
+import Demo from '../components/Filters';
 
 const CategoryPage = () => {
   const { cname } = useParams();
   const [products, setProducts] = useState<Product[]>([]);
+  const [filteredProducts, setFilteredProducts] =useState<Product[]>([]);
 
   useEffect(() => {
-    categoriesAPI.getProductsFromCategory(cname!).then((products) => {    
-      setProducts(products);
-    })
-    .catch((err) => { console.log(err); })
+    categoriesAPI
+      .getProductsFromCategory(cname!)
+      .then((products) => {
+        setProducts(products);
+        setFilteredProducts(products);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   return (
-  <ProductCardsContainer>
-    {products.map((product, index)=>{
-    return <ProductCard key={"cproduct"+index} product={product}/>})
-  }</ProductCardsContainer>
-  )
-}
+    <>
+    
+      {/* <Filter products={products} setProducts={setFilteredProducts}/> */}
+      <ProductCardsContainer>
+        {products.map((product, index) => {
+          return <ProductCard key={'cproduct' + index} product={product} />;
+        })}
+      </ProductCardsContainer>
+    </>
+  );
+};
 
 export default CategoryPage;
